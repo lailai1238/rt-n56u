994e8a9303e514b527a4b4c8eb1aefc09882b21e
dfc643 <dfc643@126.com>
2021-03-08 10:42:30 -0500

    添加 极光路由PG-AC1200L 支持

---
+++ trunk/configs/boards/PG-AC1200L/board.h
@@ -0,0 +1,28 @@
+/* FBMC PG-AC1200L */
+
+#define BOARD_PID		"PG-AC1200L"
+#define BOARD_NAME		"PG-AC1200L"
+#define BOARD_DESC		"FBMC PG-AC1200L Dual Band Wireless Router"
+#define BOARD_VENDOR_NAME	"FC-SYSTEM Inc."
+#define BOARD_VENDOR_URL	"http://www.fcsys.com/"
+#define BOARD_MODEL_URL		"http://fbmcnetwork.com/Home/PG-AC1200L"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	13
+#define BOARD_GPIO_BTN_WPS	12
+#define BOARD_GPIO_LED_ALL	10
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	9
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef	BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	1
+#define BOARD_NUM_ANT_5G_RX	1
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
