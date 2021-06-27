3070daf14db12cabfd731b225af649abfe9be523
240038901 <58842348+240038901@users.noreply.github.com>
2021-06-27 14:03:48 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -145,9 +145,9 @@ fi
 
 dl_adg(){
 logger -t "AdGuardHome" "下载AdGuardHome"
-if [ ! -f "$adgstorage/AdGuardHome/AdGuardHome" ]; then
-  wget --no-check-certificate -O $adgstorage/AdGuardHome/AdGuardHome -T 10 -c 5 https://github.com/240038901/rt-n56u/releases/download/1.0/AdGuardHome
-fi
+wget --no-check-certificate -O $adgstorage/AdGuardHome/AdGuardHome -T 10 -c 5 https://github.com/240038901/rt-n56u/releases/download/1.0/AdGuardHome
+if [ $? -ne 0 ]; then
+rm -rf /$adgstorage/AdGuardHome/AdGuardHome
 
 if [ ! -f "$adgstorage/AdGuardHome/AdGuardHome" ]; then
 logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
